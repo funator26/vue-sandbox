60624421aa345768e197be8f5b1326d97fdb4b17
kooooichi24 <ex.kouichi1224@gmail.com>
2020-07-02 15:35:32 +0900

    GETリクエストでUsersデータを取得する
    json-serverを用いてHTTPリクエストのエラーがないことを確認しました。

--- src/App.vue
+++ src/App.vue
@@ -3,19 +3,21 @@
     <Header />
     Hello {{ name }} !
     <ArticleList />
+    <Users />
   </div>
 </template>
 
 <script>
 import Header from './components/Header.vue'
-// import ArticleList from './components/ArticleList.vue'
 import ArticleList from './components/ArticleList.vue'
+import Users from './components/Users.vue'
 
 export default {
   name: 'App',
   components: {
     Header,
-    ArticleList
+    ArticleList,
+    Users
   },
   data() {
     return {
--- src/components/Users.vue
+++ src/components/Users.vue
@@ -1,12 +1,27 @@
 <template>
     <div>
         Users
+        {{ users }}
     </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
 	name: 'Users',
+    data() {
+        return {
+            users: null
+        }
+    },
+    mounted() {
+        axios
+            .get('http://localhost:3001/users')
+            .then(response => {
+                this.users = response.data
+            })
+    }
 }
 </script>
 
