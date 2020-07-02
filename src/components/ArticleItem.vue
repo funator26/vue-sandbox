9c02986cc55eba58fb7890dc240857dee95b65e1
funator26 <fff.100necdt@gmail.com>
2020-07-02 11:13:26 +0900

    記事リストの個々の要素を表すArticleItemを作成

---
+++ src/components/ArticleItem.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ArticleItem',
+    data() {
+        return {
+            articleList: null
+        }
+    },
+    mounted () {
+        axios
+        .get('http://localhost:3001/articleList')
+        .then(response => {
+            this.articleList = response.data
+      })
+  }
+}
+</script>
+
+<style scoped>
+</style>
